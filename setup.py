6f4df40b7c638bf6b3264a08af0e2addb8d2769f
Zane <zmccaig@atb.com>
2019-08-15 16:26:47 -0700

    Updated some stuff

--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
-from setuptools import setup, find_packages
-setup(
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name="storage",
-    version="0.1",
-    packages=find_packages(),
+    version="0.0.1",
+    author="Zane McCaig",
+    author_email="zmccaig@atb.com",
+    description="A python library for getting and saving files locally and in google cloud storage",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/atb-leap/storage",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
