18889d62537d795a669bd2b831303463515b7fec
Zane <zmccaig@atb.com>
2019-08-15 17:32:01 -0700

    Added requirements to setup

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
 import setuptools
 
+try:
+    # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:
+    # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
+def load_requirements(fname):
+    reqs = parse_requirements(fname, session="test")
+    return [str(ir.req) for ir in reqs]
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
@@ -11,6 +22,7 @@ setuptools.setup(
     description="A python library for getting and saving files locally and in google cloud storage",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    install_requires=load_requirements("requirements.txt"),
     url="https://github.com/atb-leap/storage",
     packages=setuptools.find_packages(),
     classifiers=[
