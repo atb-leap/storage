3e0adf05fe92d769de790ea9cdacd0ed9a7b406f
Zane <zmccaig@atb.com>
2019-08-19 17:04:14 -0700

    Added test data and more tests

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -14,9 +14,11 @@ TEST_BUCKET = 'test-lake'
 TEST_SUBDIRECTORY = 'storage_data'
 JSON_FILE = 'file1.json'
 CSV_FILE = 'file2.csv'
-FILES = [JSON_FILE, CSV_FILE]
+TSV_FILE = 'file2.tsv'
+FILES = [JSON_FILE, CSV_FILE, TSV_FILE]
 JSON_DATA = storage.json.read_json(get_local_filename(JSON_FILE))
 CSV_DATA = storage.csv.read_csv(get_local_filename(CSV_FILE))
+TSV_DATA = storage.csv.read_csv(get_local_filename(TSV_FILE))
 
 GUID = 'TODO'
 
@@ -57,6 +59,10 @@ def test_get_csv_from_gcs():
     remote_file = get_gcs_filename(CSV_FILE)
     assert_that(storage.get(remote_file)).is_equal_to(CSV_DATA)
 
+def test_get_tsv_from_gcs():
+    remote_file = get_gcs_filename(TSV_FILE)
+    assert_that(storage.get(remote_file)).is_equal_to(TSV_DATA)
+
 def test_get_json_from_local():
     local_file = get_local_filename(JSON_FILE)
     assert_that(storage.get(local_file)).is_equal_to(JSON_DATA)
@@ -65,12 +71,16 @@ def test_get_csv_from_local():
     local_file = get_local_filename(CSV_FILE)
     assert_that(storage.get(local_file)).is_equal_to(CSV_DATA)
 
+def test_get_tsv_from_local():
+    local_file = get_local_filename(TSV_FILE)
+    assert_that(storage.get(local_file)).is_equal_to(TSV_DATA)
+
 # listdir tests
 def test_listdir_gcs():
-    assert_that(storage.listdir(REMOTE_DATA_DIR)).is_equal_to(FILES)
+    assert_that(sorted(storage.listdir(REMOTE_DATA_DIR))).is_equal_to(sorted(FILES))
 
 def test_listdir_local():
-    assert_that(storage.listdir(LOCAL_DATA_DIR)).is_equal_to(FILES)
+    assert_that(sorted(storage.listdir(LOCAL_DATA_DIR))).is_equal_to(sorted(FILES))
 
 # Save tests
 def test_save_json_to_gcs(teardown):
@@ -96,3 +106,15 @@ def test_save_csv_to_local(teardown):
     teardown.delete(local_file)
     storage.save(CSV_DATA, local_file)
     assert_that(storage.get(local_file)).is_equal_to(CSV_DATA)
+
+def test_save_tsv_to_gcs(teardown):
+    remote_file = get_temp_gcs_filename(TSV_FILE)
+    teardown.delete(remote_file)
+    storage.save(CSV_DATA, remote_file)
+    assert_that(storage.get(remote_file)).is_equal_to(TSV_DATA)
+
+def test_save_tsv_to_local(teardown):
+    local_file = get_temp_local_filename(TSV_FILE)
+    teardown.delete(local_file)
+    storage.save(TSV_DATA, local_file)
+    assert_that(storage.get(local_file)).is_equal_to(TSV_DATA)
